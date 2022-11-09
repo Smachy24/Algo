eb1a35aab6837a3efa0035757c177d598c13a61e
tisma <tisma@gmail.com>
2022-11-09 12:11:10 +0100

    second

--- aaa.py
+++ aaa.py
@@ -1,9 +1,20 @@
 from random import *
 
-tableau = []
+# tableau = []
 
-for i in range(randint(1, 10)):
-    tableau.append(randint(1, 10))
+# for i in range(randint(1, 10)):
+#     tableau.append(randint(1, 10))
 
+# print('List:', tableau)
 
-print('List:', tableau)
+def triInsertion(t):
+    for i in range (len(t)):
+        cle = t[i]
+        
+        j = i-1
+        while j >= 0 and cle < t[j]:
+            t[j], t[j+1] = t[j+1], t[j]
+            j -= 1
+t = [randint(1, 50), randint(1, 50), randint(1, 50), randint(1, 50), randint(1, 50), randint(1, 50), randint(1, 50), randint(1, 50), randint(1, 50), randint(1, 50)]
+triInsertion(t)
+print (t)
