0e9eb4c9f048d00d6e6c784712e6ba75f92ae188
Smachy <pro.diegocdossantos@gmail.com>
2022-11-09 16:27:24 +0100

    modif

--- tri-bulle.py
+++ tri-bulle.py
@@ -1,37 +1,33 @@
 
 def tri_bulle(tab):
-  comparaison = 0
-  affectation = 0
+  compt=0
   for i in range(len(tab)-1):
     for j in range(len(tab)-1):
       if tab[j]>tab[j+1]:
         tab[j], tab[j+1] = tab[j+1], tab[j]
-        affectation+=3
-      comparaison+=1
-        
-  print(f"comparaison {comparaison}")
-  print(f"affectation {affectation}")
-  return tab
+        compt+=3
+      compt+=1
+  print(tab)
+  return compt
 
 def tri_bulle_optimise(tab):
-  comparaison = 0
-  affectation = 0
+  compt = 0
   stop = True
   for i in range(len(tab)-1):
     for j in range(len(tab)-1):
       if tab[j]>tab[j+1]:
         tab[j], tab[j+1] = tab[j+1], tab[j]
         stop=False
-        affectation+=3
-      comparaison+=1
+        compt+=3
+      compt+=1
     if stop==True:
       break
-  print(f"comparaison {comparaison}")
-  print(f"affectation {affectation}")
-  return tab
+  print(tab)
+  return compt
 
-#print(tri_bulle([0,1,2,3,4,5]))
-#print(tri_bulle_optimise([0,1,2,3,4,5]))
+#print(tri_bulle([0,1,2,3,4,5,6,7,8,9]))
+#print(tri_bulle_optimise([0,1,2,3,4,5,6,7,8,9]))
 
-print(tri_bulle([5,4,3,2,1,0]))
-print(tri_bulle_optimise([5,4,3,2,1,0]))
+print(tri_bulle([9,8,7,6,5,4,3,2,1,0]))
+print(tri_bulle_optimise([9,8,7,6,5,4,3,2,1,0]))
+
