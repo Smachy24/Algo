eb950454732a63b02f281e8df79eece52ec82e65
tisma <tisma@gmail.com>
2022-11-09 16:27:17 +0100

    five

--- aaa.py
+++ aaa.py
@@ -2,21 +2,19 @@ from random import *
 
 def triInsertion(t):
     comp = 0
-    affec = 0
     
     for i in range (1, len(t)):
         cle = t[i]
         
         j = i-1
-        affec+=2
-        comp+=2
+        
+        comp+=4
         while j >= 0 and cle < t[j]:
             t[j+1] = t[j]
             j -= 1
             t[j+1] = cle
-            affec += 5 
-    print('Voici le nombre de comparaisons effectuées :', comp)
-    print("Voici le nombre d'affectations effectuées :", affec)
+            comp += 5 
+    print('Voici le nombre de modifications effectuées :', comp)
     return t
 t = [randint(-10, 50), randint(-10, 50), randint(-10, 50), randint(-10, 50), randint(-10, 50), randint(-10, 50), randint(-10, 50), randint(-10, 50), randint(-10, 50), randint(-10, 50)]
 
